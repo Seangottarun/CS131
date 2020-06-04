2e8828402eaea0bb74d4dd1b5f2997bda28a41f0
Seangottarun <sean.wu20s@outlook.com>
2020-06-04 00:10:04 -0400

    Added jupyter notebook for hw0

---
+++ Assignments/hw0_release/imageManip.py
@@ -0,0 +1,206 @@
+import math
+
+import numpy as np
+from PIL import Image
+from skimage import color, io
+
+
+def load(image_path):
+    """Loads an image from a file path.
+
+    HINT: Look up `skimage.io.imread()` function.
+
+    Args:
+        image_path: file path to the image.
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    # Use skimage io.imread
+    out = io.imread(image_path)
+    ### END YOUR CODE
+
+    # Let's convert the image to be between the correct range.
+    out = out.astype(np.float64) / 255
+    return out
+
+
+def dim_image(image):
+    """Change the value of every pixel by following
+
+                        x_n = 0.5*x_p^2
+
+    where x_n is the new value and x_p is the original value.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+
+    ### YOUR CODE HERE
+    out = 0.5 * image**2
+    ### END YOUR CODE
+
+    return out
+
+
+def convert_to_grey_scale(image):
+    """Change image to gray scale.
+
+    HINT: Look at `skimage.color` library to see if there is a function
+    there you can use.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    out = color.rgb2grey(image)
+    ### END YOUR CODE
+
+    return out
+
+
+def rgb_exclusion(image, channel):
+    """Return image **excluding** the rgb channel specified
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "R", "G" or "B".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+
+    ### YOUR CODE HERE
+    colour = {'R':0, 'G':1, 'B': 2}
+    out = np.copy(image)
+    out[:, :, colour[channel]] = 0
+    ### END YOUR CODE
+
+    return out
+
+
+def lab_decomposition(image, channel):
+    """Decomposes the image into LAB and only returns the channel specified.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "L", "A" or "B".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+
+    lab = color.rgb2lab(image)
+    out = None
+
+    ### YOUR CODE HERE
+    channel_dict = {'L':0, 'A':1, 'B':2}
+    out = lab[:,:, channel_dict[channel]]
+    ### END YOUR CODE
+
+    return out
+
+
+def hsv_decomposition(image, channel='H'):
+    """Decomposes the image into HSV and only returns the channel specified.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "H", "S" or "V".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+
+    hsv = color.rgb2hsv(image)
+    out = None
+
+    ### YOUR CODE HERE
+    channel_dict = {'H':0, 'S':1, 'V':2}
+    out = hsv[:, :, channel_dict[channel]]
+    ### END YOUR CODE
+
+    return out
+
+
+def mix_images(image1, image2, channel1, channel2):
+    """Combines image1 and image2 by taking the left half of image1
+    and the right half of image2. The final combination also excludes
+    channel1 from image1 and channel2 from image2 for each image.
+
+    HINTS: Use `rgb_exclusion()` you implemented earlier as a helper
+    function. Also look up `np.concatenate()` to help you combine images.
+
+    Args:
+        image1: numpy array of shape(image_height, image_width, 3).
+        image2: numpy array of shape(image_height, image_width, 3).
+        channel1: str specifying channel used for image1.
+        channel2: str specifying channel used for image2.
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+    ### YOUR CODE HERE
+    out = np.zeros_like(image1)
+    height, width, depth = image1.shape
+    left = rgb_exclusion(image1, channel1)
+    right = rgb_exclusion(image2, channel2)
+    out = np.concatenate((left[:, :width//2, :], right[:, width//2:, :]), axis=1)
+    ### END YOUR CODE
+
+    return out
+
+
+def mix_quadrants(image):
+    """THIS IS AN EXTRA CREDIT FUNCTION.
+
+    This function takes an image, and performs a different operation
+    to each of the 4 quadrants of the image. Then it combines the 4
+    quadrants back together.
+
+    Here are the 4 operations you should perform on the 4 quadrants:
+        Top left quadrant: Remove the 'R' channel using `rgb_exclusion()`.
+        Top right quadrant: Dim the quadrant using `dim_image()`.
+        Bottom left quadrant: Brighthen the quadrant using the function:
+            x_n = x_p^0.5
+        Bottom right quadrant: Remove the 'R' channel using `rgb_exclusion()`.
+
+    Args:
+        image1: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    height, width, depth = image.shape
+
+    out = np.zeros_like(image)
+    top_left = rgb_exclusion(np.copy(image), 'R')
+    top_right = dim_image(np.copy(image))
+    bottom_left = np.copy(image)**0.5
+    bottom_right = rgb_exclusion(image, 'R')
+
+    out[:height//2, :, :] = np.concatenate((top_left[:height//2, :width//2, :], top_right[:height//2, width//2:, :]), axis=1)
+    out[height//2:, :, :] = np.concatenate((bottom_left[height//2:, :width//2, :], bottom_right[height//2:, width//2:, :]), axis=1)
+    ### END YOUR CODE
+
+    return out
---
+++ Assignments/hw0_release/linalg.py
@@ -0,0 +1,141 @@
+import numpy as np
+
+
+def dot_product(a, b):
+    """Implement dot product between the two vectors: a and b.
+
+    (optional): While you can solve this using for loops, we recommend
+    that you look up `np.dot()` online and use that instead.
+
+    Args:
+        a: numpy array of shape (x, n)
+        b: numpy array of shape (n, x)
+
+    Returns:
+        out: numpy array of shape (x, x) (scalar if x = 1)
+    """
+    out = None
+    ### YOUR CODE HERE
+    out = np.dot(a, b)
+    ### END YOUR CODE
+    return out
+
+
+def complicated_matrix_function(M, a, b):
+    """Implement (a * b) * (M * a.T).
+
+    (optional): Use the `dot_product(a, b)` function you wrote above
+    as a helper function.
+
+    Args:
+        M: numpy matrix of shape (x, n).
+        a: numpy array of shape (1, n).
+        b: numpy array of shape (n, 1).
+
+    Returns:
+        out: numpy matrix of shape (x, 1).
+    """
+    out = None
+    ### YOUR CODE HERE
+    out = dot_product(a, b) * dot_product(M, a.T)
+    ### END YOUR CODE
+
+    return out
+
+
+def svd(M):
+    """Implement Singular Value Decomposition.
+
+    (optional): Look up `np.linalg` library online for a list of
+    helper functions that you might find useful.
+
+    Args:
+        M: numpy matrix of shape (m, n)
+
+    Returns:
+        u: numpy array of shape (m, m).
+        s: numpy array of shape (k).
+        v: numpy array of shape (n, n).
+    """
+    u = None
+    s = None
+    v = None
+    ### YOUR CODE HERE
+    u, s, v = np.linalg.svd(M)
+    ### END YOUR CODE
+
+    return u, s, v
+
+
+def get_singular_values(M, k):
+    """Return top n singular values of matrix.
+
+    (optional): Use the `svd(M)` function you wrote above
+    as a helper function.
+
+    Args:
+        M: numpy matrix of shape (m, n).
+        k: number of singular values to output.
+
+    Returns:
+        singular_values: array of shape (k)
+    """
+    singular_values = None
+    ### YOUR CODE HERE
+    U, S, V = svd(M)
+    singular_values = S[0:k]
+    ### END YOUR CODE
+    return singular_values
+
+
+def eigen_decomp(M):
+    """Implement eigenvalue decomposition.
+
+    (optional): You might find the `np.linalg.eig` function useful.
+
+    Args:
+        matrix: numpy matrix of shape (m, n)
+
+    Returns:
+        w: numpy array of shape (m, m) such that the column v[:,i] is the eigenvector corresponding to the eigenvalue w[i].
+        v: Matrix where every column is an eigenvector.
+    """
+    w = None
+    v = None
+    ### YOUR CODE HERE
+    w, v = np.linalg.eig(M)
+    ### END YOUR CODE
+    return w, v
+
+
+def get_eigen_values_and_vectors(M, k):
+    """Return top k eigenvalues and eigenvectors of matrix M. By top k
+    here we mean the eigenvalues with the top ABSOLUTE values (lookup
+    np.argsort for a hint on how to do so.)
+
+    (optional): Use the `eigen_decomp(M)` function you wrote above
+    as a helper function
+
+    Args:
+        M: numpy matrix of shape (m, m).
+        k: number of eigen values and respective vectors to return.
+
+    Returns:
+        eigenvalues: list of length k containing the top k eigenvalues
+        eigenvectors: list of length k containing the top k eigenvectors
+            of shape (m,)
+    """
+    eigenvalues = []
+    eigenvectors = []
+    ### YOUR CODE HERE
+    w, v = eigen_decomp(M)
+    asc_evals_args = np.argsort(np.abs(w))
+    evals_args = asc_evals_args[::-1]
+
+    sorted_eigenvalues = w[evals_args]
+    sorted_eigenvectors = v[:,evals_args]
+
+    eigenvalues = sorted_eigenvalues
+    eigenvectors = sorted_eigenvectors[:k]
+    ### END YOUR CODE
+    return eigenvalues, eigenvectors
